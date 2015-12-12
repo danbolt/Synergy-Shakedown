519b1799331b38cc68d762351787ec5df80086f1
Daniel Savage <dansvg@gmail.com>
2015-12-12 13:45:29 +0900

    Double tap to change directions

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,11 +1,12 @@
 var Gameplay = function () {};
 Gameplay.prototype = {
   motivateKey: null,
+  moveKey: null,
 
   developers: null,
   player: null,
 
-  developerCount: 4,
+  developerCount: 6,
   playerMoveSpeed: 200,
   playerCloseDistance: 8,
   initialDevMotivation: 20,
@@ -14,6 +15,8 @@ Gameplay.prototype = {
   motivationPerPress: 1.5,
 
   targetPlayerIndex: 0,
+  movingForward: false,
+  timeSinceLastDownPress: 0,
 
   motivateDev: function () {
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.X) || this.game.input.gamepad.pad1.isDown(Phaser.Gamepad.BUTTON_5)) {
@@ -32,6 +35,16 @@ Gameplay.prototype = {
     }
   },
 
+  reverseDirection: function () {
+    if (this.timeSinceLastDownPress < 500) {
+      this.movingForward = !this.movingForward;
+
+      this.targetPlayerIndex = (this.movingForward ? (this.targetPlayerIndex + 1) : (this.targetPlayerIndex - 1 + this.developers.length)) % this.developers.length;
+    }
+
+    this.timeSinceLastDownPress = 0;
+  },
+
   create: function () {
     this.game.stage.backgroundColor = '#DDDDDD';
 
@@ -39,14 +52,16 @@ Gameplay.prototype = {
     this.game.physics.arcade.enable(this.player);
     this.player.body.setSize(16, 16);
 
-    var motivateKey = this.game.input.keyboard.addKey(Phaser.KeyCode.C);
-    motivateKey.onDown.add(this.motivateDev, this);
-    this.game.input.gamepad.onDownCallback = function (buttonCode) { if (buttonCode !== 4) { return; } this.motivateDev();};
+    this.motivateKey = this.game.input.keyboard.addKey(Phaser.KeyCode.C);
+    this.motivateKey.onDown.add(this.motivateDev, this);
+    this.moveKey = this.game.input.keyboard.addKey(Phaser.KeyCode.X);
+    this.moveKey.onDown.add(this.reverseDirection, this);
+    this.game.input.gamepad.onDownCallback = function (buttonCode) { if (buttonCode === 4) { this.motivateDev(); } if (buttonCode === 5) { this.reverseDirection(); } };
     this.game.input.gamepad.callbackContext = this;
 
     this.developers = [];
     for (var i = 0; i < this.developerCount; i++) {
-      var newDev = this.game.add.sprite(~~(this.game.width / 2 + 100 * (Math.cos(i / this.developerCount * Math.PI * 2))), ~~(this.game.height / 2 + 60 * (Math.sin(i / this.developerCount * Math.PI * 2))), null);
+      var newDev = this.game.add.sprite(~~(this.game.width / 2 + 100 * (Math.cos(i / this.developerCount * Math.PI * 2))), ~~(this.game.height / 2 + 70 * (Math.sin(i / this.developerCount * Math.PI * 2))), null);
       this.game.physics.arcade.enable(newDev);
       newDev.body.setSize(16, 16);
       newDev.motivation = 20;
@@ -54,22 +69,29 @@ Gameplay.prototype = {
 
       this.developers.push(newDev);
     }
+
+    this.targetPlayerIndex = 0;
+    this.movingForward = true;
+    this.timeSinceLastDownPress = 0;
   },
   update: function () {
+    this.timeSinceLastDownPress += this.game.time.physicsElapsedMS;
     // move toward the next developer
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.X) || this.game.input.gamepad.pad1.isDown(Phaser.Gamepad.BUTTON_5)) {
-      Phaser.Point.subtract(this.developers[this.targetPlayerIndex].position, this.player.position, this.player.body.velocity);
+      var target = this.developers[this.targetPlayerIndex];
+
+      Phaser.Point.subtract(target.position, this.player.position, this.player.body.velocity);
       this.player.body.velocity = Phaser.Point.normalize(this.player.body.velocity);
       this.player.body.velocity.setMagnitude(this.playerMoveSpeed);
     } else {
       this.player.body.velocity.set(0);
     }
 
-    // if the player gets super close to the developer, switch the target to the next one
+    // if the player gets super close to the developer or a developer stops working, switch the target to the next one
     if (this.developers[this.targetPlayerIndex].alive === false || Phaser.Point.distance(this.player.position, this.developers[this.targetPlayerIndex].position) < this.playerCloseDistance) {
       var loopLimiter = 0;
       do {
-        this.targetPlayerIndex = (this.targetPlayerIndex + 1) % this.developers.length;
+        this.targetPlayerIndex = (this.movingForward ? (this.targetPlayerIndex + 1) : (this.targetPlayerIndex - 1 + this.developers.length)) % this.developers.length;
         loopLimiter++;
       } while (loopLimiter < this.developers.length && this.developers[this.targetPlayerIndex].alive === false);
     }
@@ -102,5 +124,7 @@ Gameplay.prototype = {
 
     this.motivateKey.removeAll();
     this.motivateKey = null;
+    this.moveKey.removeAll();
+    this.moveKey = null;
   }
 };
